ffa422983bb03d41add9316a831d4763793bcddd
s3822042 <s3822042@rmit.edu.vn>
2021-07-24 18:03:27 +0700

    change ++

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
+
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Log in | Deviant Art");
+   }
 
   ngOnInit(): void {
   }
